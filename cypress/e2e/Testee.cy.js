d7e5aff51364676f80bc6ac32deb2c8dc3fc7074
posalva <salvalagioo@gmail.com>
2025-02-04 22:26:56 -0300

    primeiro commit

---
+++ cypress/e2e/Testee.cy.js
@@ -0,0 +1,8 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://www.facebook.com')
+      cy.get('.ytSearchboxComponentInput').click().type('Type beat Japa')
+      cy.get('.ytSearchboxComponentSearchButton').click()
+      cy.contains('{FREE} Type Beat Japa').click()
+  })
+})
